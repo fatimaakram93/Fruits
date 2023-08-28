57eb68f068e91b5668b649c5478b7bd20840b9ea
Fatima <fatima.akram@saitow.ag>
2023-08-28 11:47:26 +0200

    Setup Xcode Project

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Fatima Akram on 28.08.23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
